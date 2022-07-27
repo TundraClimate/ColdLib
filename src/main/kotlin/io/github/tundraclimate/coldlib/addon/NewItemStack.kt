96ca2f1f22a868bb44cc0e0e8ff742c3df057aec
Tundra_Climate <tundra.tundere@gmail.com>
2022-07-27 09:34:14 +0900

    bug fix

--- src/main/kotlin/io/github/tundraclimate/coldlib/addon/NewItemStack.kt
+++ src/main/kotlin/io/github/tundraclimate/coldlib/addon/NewItemStack.kt
@@ -2,69 +2,46 @@ package io.github.tundraclimate.coldlib.addon
 
 import com.google.common.collect.Multimap
 import org.bukkit.Material
+import org.bukkit.NamespacedKey
 import org.bukkit.attribute.Attribute
 import org.bukkit.attribute.AttributeModifier
 import org.bukkit.enchantments.Enchantment
 import org.bukkit.inventory.ItemFlag
 import org.bukkit.inventory.ItemStack
-import org.bukkit.persistence.PersistentDataContainer
+import org.bukkit.persistence.PersistentDataType
 
-class NewItemStack(material: Material) {
-    val item = ItemStack(material)
-    private val meta = item.itemMeta
-    var displayName: String? = meta?.displayName
-        set(value) {
-            field = value
-            item.itemMeta = meta
-        }
-    var lore: MutableList<String>? = meta?.lore
-        set(value) {
-            field = value
-            item.itemMeta = meta
-        }
-    var enchants: MutableMap<Enchantment, Int>? = meta?.enchants
-        set(value) {
-            field = value
-            item.itemMeta = meta
-        }
+class NewItemStack(private val material: Material) {
+    var displayName: String? = null
+    var lore: List<String>? = null
     var customModelData: Int? = null
-        set(value) {
-            field = value
-            item.itemMeta = meta
-        }
-    var itemFlags: MutableSet<ItemFlag>? = meta?.itemFlags
-        set(value) {
-            field = value
-            item.itemMeta = meta
-        }
-    var isUnbreakable: Boolean? = meta?.isUnbreakable
-        set(value) {
-            field = value
-            item.itemMeta = meta
-        }
-    var attributeModifiers: Multimap<Attribute, AttributeModifier>? = meta?.attributeModifiers
-        set(value) {
-            field = value
-            item.itemMeta = meta
-        }
-    var localizedName: String? = meta?.localizedName
-        set(value) {
-            field = value
-            item.itemMeta = meta
-        }
-    var dataContainer: PersistentDataContainer? = meta?.persistentDataContainer
-        set(value) {
-            field = value
-            item.itemMeta = meta
-        }
-    var amount: Int = item.amount
-        set(value) {
-            field = value
-            item.amount = value
-        }
-    var type: Material = item.type
-        set(value) {
-            field = value
-            item.type = value
+    var localizeName: String? = null
+    var enchants = mutableMapOf<Enchantment, Int>()
+    var isUnbreakable = false
+    var itemFlags = arrayOf<ItemFlag>()
+    var attributeModifiers: Multimap<Attribute, AttributeModifier>? = null
+    var dataContainer: List<Triple<NamespacedKey, PersistentDataType<String, String>, String>> = listOf()
+    var amount = 1
+    var type = material
+    val item: ItemStack
+        get() {
+            val itemStack = ItemStack(material)
+            val meta = itemStack.itemMeta
+            meta?.let { m ->
+                m.setDisplayName(displayName)
+                m.lore = lore
+                m.setCustomModelData(customModelData)
+                m.setLocalizedName(localizeName)
+                m.isUnbreakable = isUnbreakable
+                m.addItemFlags(*itemFlags)
+                m.attributeModifiers = attributeModifiers
+                dataContainer.forEach {
+                    m.persistentDataContainer.set(it.first, it.second, it.third)
+                }
+                itemStack.setItemMeta(m)
+            }
+            itemStack.addUnsafeEnchantments(enchants)
+            itemStack.amount = amount
+            itemStack.type = type
+            return itemStack
         }
 }
